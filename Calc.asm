87fb9416bc1fc6799c98e958984857b928ec4139
mahmoud khalil <khalil2535@protonmail.com>
2019-05-24 16:06:46 +0300

    more modulraization
    Signed-off-by: mahmoud khalil <khalil2535@protonmail.com>

--- Calc.asm
+++ Calc.asm
@@ -2,6 +2,7 @@ dosseg
 .model small
 .data
     greating_msg db "Welcome in 8086-Calculator type simple equations to solve",10,"   Examples : 1+1 or 5*8",10,"$"
+    error_msg db 10,"Exit due error$"
     input db 3 DUP('0')
 .code
     ; printing greating_msg
@@ -10,85 +11,86 @@ dosseg
     
     mov ah,9
     mov dx,offset greating_msg
-    int '!' ;21h = '!'
+    int '!'         ; '!' = 21h
     
     mov bx,0
-    call take_input
-    call calculate_input
-    
-    ; Quit the program
-    quit:
-    mov ah,'L'
-    int '!'
-
-    take_input:                        ; loop to take input and print dl for each letter
-    mov ah,8
+take_input:                        ; loop to take input and print dl for each letter
+    mov ah,8        ; to input 1 character
     int '!'
     mov cl,al
-    cmp bx,1
+    cmp bx,1        ; if it's the second character then it's opreation
     je check_opreation
-    cmp cl,48                          ; 13 in decimal is the ascii for enter
-    jb quit_take_input                 ; to stop looping
-    cmp cl,57
-    ja quit_take_input
+
+check_number: ; check if the input is a number (more or equal to 0 and less or equal to 9)
+    cmp cl,'0'
+    jb print_error_and_quit          ; to stop looping if error
+    cmp cl,'9'
+    ja print_error_and_quit          ; to stop looping if error
     jmp assign
-    
-    check_opreation:
+
+check_opreation:
     cmp cl,'+'
     je assign
     cmp cl,'-'
     je assign
-    
-    ; TODO support the following oprations
-    ;cmp cl,'*'
+    ;cmp cl,'*' ; TODO support the following oprations
     ;je assign
     ;cmp cl,'/'
     ;je assign
-    
-    assign:
+    jmp print_error_and_quit
+
+assign:
     call print_cl
     mov input[bx],cl
     inc bx
     cmp bx,3
     jne take_input
-    quit_take_input:
-    ret
-    
-    calculate_input:
+
+calculate_input:
     mov cl,input[0]         ; First number
     mov bl,input[1]         ; the mark between the numbers
     mov ch,input[2]          ; Second number
     cmp bl,'+'
-    je add_and_quit
+    je adding
     cmp bl,'-'
-    je sub_and_quit
-    ret
+    je subtracting
+    ; TODO support the following oprations
+    ;cmp bl,'*'
+    ;je multiplying
+    ;cmp bl,'/'
+    ;je dividing
+print_error_and_quit:
+    mov ah,9
+    mov dx,offset error_msg
+    int '!'         ; '!' = 21h
     
-    add_and_quit:
+quit: ; Quit the program
+    mov ah,'L'  ; 'L' = 4Ch
+    int '!'
+    
+adding:
     call print_equal_sign
     sub ch,48
     add cl,ch
     call print_cl
-    call quit
-    ret
+    jmp quit
     
-    sub_and_quit:
+subtracting:
     call print_equal_sign
     sub ch,48
     sub cl,ch
     call print_cl
-    call quit
-    ret
+    jmp quit
     
-    print_equal_sign:
+print_equal_sign:
     mov ah,2
     mov dl,'='
     int '!'
     ret
     
-    print_cl:
+print_cl:
     mov ah,2
     mov dl,cl
     int '!'
     ret
-    end
+end
